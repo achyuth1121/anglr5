eb6bac6135113322d76ed2cb31f85fb859bf2746
achyuth1121 <49063739+achyuth1121@users.noreply.github.com>
2021-03-17 12:14:56 +0530

    Add files via upload

---
+++ src/app/components/cost/cost.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../service/api/api-services.service';
+import { Router } from '@angular/router';
+import { SessionService } from '../../service/session/session.service';
+
+
+@Component({
+  selector: 'app-cost',
+  templateUrl: './cost.component.html',
+  styleUrls: ['./cost.component.scss']
+})
+export class CostComponent implements OnInit {
+
+  //subcription = new FormControl();
+
+  subscriptionlist: any = [];
+  istargetApp: boolean = false;
+  selectedSubscription: any = [];
+  isLoading: boolean;
+  value: number = 0;
+
+  constructor(private api: ApiService, private router: Router, private session: SessionService) { }
+
+  ngOnInit() {
+    const self = this;
+    self.isLoading = true;
+
+    let interval = setInterval(() => {
+        this.value = this.value + Math.floor(Math.random() * 10) + 1;
+        if (this.value >= 100) {
+            this.value = 100;            
+            clearInterval(interval);
+        }
+    }, 500);
+
+    self.api.getSubscriptions().then((data: any) => {
+      self.subscriptionlist = data;
+      self.istargetApp = true;
+      self.isLoading = false;
+    }, (err) =>  {});
+  }
+
+  getCostDetails() {
+    const self = this;
+    self.session.subscriptionID = self.selectedSubscription;
+    self.router.navigate(['costDetails']);
+  }
+
+}
